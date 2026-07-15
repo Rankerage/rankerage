99dfb03fff70f1e5e48773f314deb0ac2f05f2ce
Simon Saint <saint@rankerage.com>
2026-07-15 12:53:42 +0900

    feat: expand trend chart to 17 metrics with historical data

--- docs/js/table.js
+++ docs/js/table.js
@@ -1348,7 +1348,7 @@
     });
 
     // Trend chart for GDP, Population, Life Exp, GDP/cap
-    var trendFields = {"gdp":1,"gdp_per_capita":1,"population":1,"life_expectancy":1,"urban_pop":1,"internet_pct":1,"military_pct":1,"health":1,"fertility":1,"forest":1,"edu":1,"rd":1,"poverty":1};
+    var trendFields = {"gdp":1,"gdp_per_capita":1,"population":1,"life_expectancy":1,"urban_pop":1,"internet_pct":1,"military_pct":1,"health":1,"fertility":1,"forest":1,"edu":1,"rd":1,"poverty":1,"birth_rate":1,"death_rate":1,"infant_mortality":1,"exports":1};
     var historyData = null;
     var chartInstance = null;
     var chartModal = document.getElementById("chartModal");
